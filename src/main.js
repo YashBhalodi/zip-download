1a5eea19872975792dfed2aa3c5e2ce260e9549a
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-27 22:52:24 +0530

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,99 @@
-import './index.css'
+import "./index.css";
+import JSZip from "jszip";
+
+const files = [
+  {
+    name: "StudentA",
+    type: "FOLDER",
+    children: [
+      {
+        url: "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits",
+        type: "FILE",
+        name: "commits.json",
+      },
+    ],
+  },
+  {
+    name: "StudentB",
+    type: "FOLDER",
+    children: [
+      {
+        url: "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits",
+        type: "FILE",
+        name: "commits.json",
+      },
+    ],
+  },
+  {
+    name: "StudentC",
+    type: "FOLDER",
+    children: [
+      {
+        url: "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits",
+        type: "FILE",
+        name: "commits.json",
+      },
+    ],
+  },
+];
+
+const getProcessedFiles = (files, parentPath = "") => {
+  const result = [];
+  files.forEach((file) => {
+    if (file.type === "FILE") {
+      result.push({
+        ...file,
+        path: parentPath ? parentPath + "/" + file.name : file.name,
+      });
+    } else {
+      const { children, ...rest } = file;
+      const path = parentPath ? parentPath + "/" + file.name : file.name;
+      result.push({
+        ...rest,
+        path,
+      });
+      result.push(...getProcessedFiles(children, path));
+    }
+  });
+  return result;
+};
+
+const downloadBlob = (blob, name) => {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.download = name;
+  link.href = url;
+  link.click();
+  URL.revokeObjectURL(url);
+};
+
+const generateZIP = () => {
+  const zip = new JSZip();
+  const normalizedFiles = getProcessedFiles(files);
+  const folders = normalizedFiles.filter((file) => file.type === "FOLDER");
+  const filesToDownload = normalizedFiles.filter(
+    (file) => file.type === "FILE"
+  );
+
+  // first create all the necessary folders
+  folders.forEach((folder) => {
+    zip.folder(folder.path);
+  });
+
+  // promise array that download and fetch files as blob and adds it to zip
+  const fileDownloadPromiseArr = filesToDownload.map((file) =>
+    fetch(file.url)
+      .then((res) => res.blob())
+      .then((blob) => zip.file(file.path, blob))
+  );
+
+  Promise.all(fileDownloadPromiseArr).then(() => {
+    zip.generateAsync({ type: "blob" }).then(function (content) {
+      downloadBlob(content, "bundle.zip");
+    });
+  });
+};
+
+document
+  .querySelector("#normal_download")
+  .addEventListener("click", generateZIP);
